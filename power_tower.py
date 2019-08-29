b50c739d0f20a48943461b7b6ee2280995e48997
Andrew Webb <andrew@awebb.info>
2019-08-29 21:28:52 +0100

    .

--- power_tower.py
+++ power_tower.py
@@ -5,10 +5,12 @@ from utils import get_colour, is_close
 
 
 # Vector of initial points in the complex plane
-re_min, re_max = (-3.5, 5.5)
+re_min, re_max = (-3.5, 5.0)
+#im_min, im_max = (-1.416666667, 1.4166666667)
 im_min, im_max = (-4.5, 4.5)
-resolution = 200
-initial_zs = (np.expand_dims(np.linspace(re_min, re_max, resolution), 0) + 1j * np.expand_dims(np.linspace(im_min, im_max, resolution), 1)).flatten()
+h_resolution = 300
+v_resolution = int(h_resolution * (im_max - im_min) / (re_max - re_min))
+initial_zs = (np.expand_dims(np.linspace(re_min, re_max, h_resolution), 0) + 1j * np.expand_dims(np.linspace(im_min, im_max, v_resolution), 1)).flatten()
 
 # Iterate raising to the power
 iterations = 200
@@ -32,7 +34,7 @@ for i in range(1, record_history_length, 1):
     period[np.logical_and(period == 0, close)] = i
 
 # Image array, initially white
-rgb = np.ones((resolution * resolution, 3)) * 255
+rgb = np.ones((h_resolution * v_resolution, 3)) * 255
 # Diverging points are black
 rgb[~np.isfinite(zs)] = np.array([0, 0, 0])
 # Colour points for each periodicity
@@ -41,6 +43,7 @@ for i in range(1, record_history_length, 1):
 
 # Save the image
 im = Image.fromarray(
-    rgb.astype(np.uint8).reshape(resolution, resolution, 3)
+    rgb.astype(np.uint8).reshape(v_resolution, h_resolution, 3)
 )
+im = im.resize((h_resolution // 2, v_resolution // 2), Image.BILINEAR)
 im.save('power_tower.png')
